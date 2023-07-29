55f2bbd6607bb6082d9675331ebc7ce0ba838f0a
Nir <niritzhak10.devel@gmail.com>
2023-07-29 11:42:32 +0300

    06-53: Implement Creating New Tour

--- app.js
+++ app.js
@@ -19,3 +19,55 @@ app.get('/api/v1/tours', (req, res) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+/**Middle ware to put the request body on the Request object
+ * without it - the req.body is undefined!
+ */
+app.use(express.json());
+
+app.post('/api/v1/tours', (req, res) => {
+  //Generate the id(The id is part of the API STATE?)
+  const newId = tours[tours.length - 1].id + 1;
+
+  //Create the new tour by merging the id with the req.bod
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  //Update the STATE - add the new tour to the tours array
+  tours.push(newTour);
+
+  /*PERSIST THE NEW TOUR INTO THE FILE(ASYNC)
+    Before persisting the tours JSON object into the json file(text file)
+   
+   -  Must Stringify it into a String format
+   
+   - NOTE: Build the respone object inside the callback function which 
+          will be executed as soon as the writing into the file completed
+          
+          the data property of the response is the ENVELOP
+      
+   */
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  console.log(tours);
+
+  //WHAT IS WRONG??
+  //JSON.parse(tour);
+  //   const tourJSON = JSON.parse(tour);
+  //   tours.add(tour);
+  //Persist the req.body into the file
+  //   fs.writeFileSync(`${__dirname}/dev-data/data`, tour, (err) => {
+  //     console.error('Could not persist the tour');
+  //   });
+  //res.send('DONE');
+});
