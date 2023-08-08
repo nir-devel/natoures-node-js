76c56f1eb8cacf1cb972f3ef54ee780dba715262
Nir <niritzhak10.devel@gmail.com>
2023-08-08 11:35:48 +0300

    105:Implementing Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 // Create the tourschema with mongoose(same data types as native JS)
 // const tourSchema = mongoose.Schema({
 //   name: {
@@ -78,8 +79,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      // maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      //minlength: [10, 'A tour name must have more or equal then 10 characters'],
       // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
@@ -94,16 +95,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
-      enum: {
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult',
-      },
+      // enum: {
+      //   values: ['easy', 'medium', 'difficult'],
+      //   message: 'Difficulty is either: easy, medium, difficult',
+      // },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0'],
+      // min: [1, 'Rating must be above 1.0'],
+      // max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -157,6 +158,36 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+///////////////////////////////////////////
+//DOCUMENT MIDDLEARE
+
+// can be executed on the 'save' and 'create' BUT NOT ON THE insertMany, findByIDAndUpdate, etc...!!
+//
+tourSchema.pre('save', function (next) {
+  //console.log('SAVED WAS CALLED... ABOUT THE PERSIST');
+  //OK : print the current processed Document
+  //console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  console.log(this);
+  //SINCE I HAVE ANOTHER PRE HOOK - I MUST CALL NEXT!
+  next();
+});
+
+//Second Middleware function on the same PRE HOOK(HOOK = 'save')
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document....');
+
+//   next();
+// });
+
+// tourSchema.post('save', function (docs, next) {
+//   // console.log('inside post middleware');
+//   // console.log(docs);
+//   //THIS IS THE LAST  HOOK IN THE MIDDLWARE STACK - I DONT HAVE TO CALL NEXT
+//   //BUT GOOD PRACTIEC(the request is stuck ! but the tour has been persisted)
+//   next();
+// });
 
 /**
    * Create a tour model from the shcema
