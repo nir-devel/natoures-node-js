2bdec2eaae35f67d27ccf1ff83cf3276e5b1a5ca
Nir <niritzhak10.devel@gmail.com>
2023-08-10 11:59:18 +0300

    114:Implementing-global-error-handling-middleware-function-in-app.js

--- app.js
+++ app.js
@@ -55,12 +55,27 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //IF I REACH HERE - THE REQUEST RESPONSE HAS NOT BEEN FINISHED -
+
 //STEP 1 TO HANDLE UNHANDLED ROUTES
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
-  next();
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+});
+
+//EXPRESS ERROR HANDLING MIDDLEWARE FUNCTION- 4 params
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+
+  // next(err);
 });
 module.exports = app;
