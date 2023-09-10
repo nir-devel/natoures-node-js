3597682e553974830b30f753044d5fd307b0fea3
Nir <niritzhak10.devel@gmail.com>
2023-09-10 12:38:55 +0300

    182:Building-The-Tour-Overview-Template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,16 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+//must add next() to make the catchAsync works!
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1 Get all tours data from the collection in db
+  const tours = await Tour.find();
+
+  // 2 Build template(not here in the controller)
+
+  // 3 Render that tempalte using the tour data from 1)
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
