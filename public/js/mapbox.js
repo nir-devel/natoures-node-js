b82379b5839e11d9ea28b385489a7a65878ab80e
Nir <niritzhak10.devel@gmail.com>
2023-09-11 22:49:46 +0300

    186: Including Map With Mapbox Part 1

---
+++ public/js/mapbox.js
@@ -0,0 +1,9 @@
+//DISABLE ESLING FOR THE ENTIRE FILE(SINCE ESLINT CONFIUGRED FOR NODE JS - NOT JS CLIENT)
+console.log('HELLO FROM THE FRONT END');
+
+//READ THE locations data from the HTML data attribute:
+//Reading the data-attribute(data-locations) from the HTML
+// const locations = document.getElementById('map').dataset.locations;
+const locations = document.getElementById('map').dataset.locations;
+
+console.log(locations);
